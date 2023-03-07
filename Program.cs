cfbe5e1d0be1f57d453108eb027fe3bcde170d65
incimeltemd <incimeltemdaglaroglu@gmail.com>
2023-03-07 23:11:39 +0300

    ödev yapılldı

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿static void Main(string[] args)
+{
+    ////normal
+    Calisan calisan1=new Calisan();
+
+    calisan1.Ad="Meltem";
+    calisan1.Soyad="Dağlaroğlu";
+    calisan1.No=949997;
+    calisan1.Departman="Halkla ilişkiler";
+
+    calisan1.CalisanBilgileri();
+    
+    //constructor
+    Calisan calisan2= new Calisan("meltem","can",979994,"bilişim");
+    calisan2.CalisanBilgileri();
+
+}
+class Calisan
+{
+    public Calisan(){}
+    public Calisan(string ad, string soyad, int no, string departman)
+    {
+        this.Ad=ad;
+        this.Soyad=soyad;
+        this.No=no;
+        this.Departman=departman;
+    }
+    public string Ad;
+    public string Soyad;
+    public int No;
+    public string Departman;
+
+    public void CalisanBilgileri()
+    {
+        Console.WriteLine("Ad={0}",Ad);
+        Console.WriteLine("Soyad={0}",Soyad);
+        Console.WriteLine("Çalışan No={0}",No);
+        Console.WriteLine("Departman=",Departman);
+
+    }
+
+}
+////constructor
